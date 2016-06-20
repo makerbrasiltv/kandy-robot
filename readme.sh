7e1fde4c4e1c25bf2a01835718a4305cde423e76
Alex Costa <makerbrasiltv@gmail.com>
2016-06-20 10:44:27 -0300

    readme for shell bash

---
+++ readme.sh
@@ -0,0 +1,34 @@
+ _    _                _          ______        _                
+| |  / )              | |        (_____ \      | |          _    
+| | / / ____ ____   _ | |_   _    _____) ) ___ | | _   ___ | |_  
+| |< < / _  |  _ \ / || | | | |  (_____ ( / _ \| || \ / _ \|  _) 
+| | \ ( ( | | | | ( (_| | |_| |        | | |_| | |_) ) |_| | |__ 
+|_|  \_)_||_|_| |_|\____|\__  |        |_|\___/|____/ \___/ \___)
+                        (____/                                   
+
+#kandy-robot
+
+##Versão em Português Brasileiro
+
+Esta é a primeira versão para raspberry pi, com python e GPIO.
+Este repositório trata-se de um robô inteligente que interage com pessoas em um ambiente.
+
+Objetivo deste projeto é estimular, crianças, jovens e simpatizantes do assunto, desenvolver robô de forma facil e didático.
+ 
+Junte-se a nós.
+Seja bem vindo.
+
+##English version
+
+This is the first version for raspberry pi with python and GPIO.
+This repository it is an intelligent robot that interacts with people in a room.
+
+Objective of this project is to encourage children, young people and supporters of the subject, develop robot easy and didactic way.
+ 
+Join us.
+Welcome.
+
+
+
+
+75 97 110 100 121  82 111 98 111 116 
